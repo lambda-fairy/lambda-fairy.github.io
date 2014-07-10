438a30744d452ae85d38231ec58740fcf9b5bfba
Chris Wong <lambda.fairy@gmail.com>
2014-07-10 15:52:12 +1200

    Shuffle images around

--- site.hs
+++ site.hs
@@ -15,7 +15,7 @@ import Templates
 main :: IO ()
 main = (getConfig >>=) . flip hakyllWith $ do
     -- Copy static files unmodified
-    match ("CNAME" .||. "images/*") $ do
+    match ("CNAME" .||. "images/**") $ do
         route   idRoute
         compile copyFileCompiler
 
