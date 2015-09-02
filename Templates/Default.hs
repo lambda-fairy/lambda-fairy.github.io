8249ada9a23f5e521ab18ea1806c9aa83698b343
Chris Wong <lambda.fairy@gmail.com>
2015-09-02 21:25:38 +1200

    Revert "Add Mixpanel"
    This reverts commit 37c49f27f7c24cc32fa46116380951b6dfb3d248.

--- Templates/Default.hs
+++ Templates/Default.hs
@@ -6,7 +6,6 @@ module Templates.Default (defaultTemplate) where
 import Control.Applicative
 import Control.Monad
 import Data.Monoid
-import System.IO.Unsafe
 
 import Templates.Core
 
@@ -29,7 +28,6 @@ defaultTemplate = LucidTemplate $ \ask -> do
             stylesheet "/styles/styles.css"
             stylesheet "//fonts.googleapis.com/css?family=Cabin:400,700,400italic,700italic"
             meta_ [name_ "viewport", content_ "width=device-width"]
-            toHtmlRaw mixpanel
 
         body_ $ do
             header_ $ h1_ $ a_ [href_ "/"] "lambda fairy"
@@ -60,8 +58,3 @@ defaultLinks
     : link "/blog" "Blog"
     : link "https://github.com/lfairy" "GitHub"
     : []
-
-
-mixpanel :: String
-mixpanel = unsafePerformIO $ readFile "mixpanel.html"
-{-# NOINLINE mixpanel #-}
